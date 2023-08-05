da613b966946de9624d3cf76e463141e93e6b8f0
mjaoanan <137941785+mjaoanan@users.noreply.github.com>
2023-08-04 22:38:48 -0700

    Update Calc summary.R
    Finished with Summary Info calculation

--- Calc summary.R
+++ Calc summary.R
@@ -1,1 +1,18 @@
 
+library(dplyr)
+
+
+data <- read.csv("cities.csv")
+View(data)
+
+stateWithoutClinic <- data %>%
+  filter(gestation_8_duration > 0)
+View(stateWithoutClinic)
+CitiesWithoutClinicAccessIn8hrs <- nrow(stateWithoutClinic)
+
+totalPeople <- sum(stateWithoutClinic$population)
+
+longestDriveforCitiesIf8 <- max(stateWithoutClinic$gestation_8_duration)
+
+CitiesWithLongestDrive <- stateWithoutClinic <- data %>%
+  filter(gestation_8_duration == longestDriveforCitiesIf8)

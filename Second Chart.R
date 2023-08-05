ff311c8c59fc8e26aa36f7a02d6c05580ca36601
mjaoanan <137941785+mjaoanan@users.noreply.github.com>
2023-08-04 23:31:18 -0700

    Update Second Chart.R
    Implemented 2nd chart

--- Second Chart.R
+++ Second Chart.R
@@ -1,1 +1,19 @@
 
+library(ggplot2)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+
+
+data <- read.csv("cities.csv")
+
+dataCompare <- data %>%
+  filter(state == "Alabama") %>%
+  select(city, gestation_8_duration, gestation_12_duration, gestation_16_duration, gestation_20_duration) %>%
+  gather (key = Weeks, value = Duration , -city)
+
+alabamaGraph <- ggplot(dataCompare) +
+  geom_col(
+    mapping = aes(x = city, y = Duration, fill = Weeks), position = "dodge"
+  )
+
